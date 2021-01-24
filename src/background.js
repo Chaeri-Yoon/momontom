ad759fa3bb0f7ce3d5e6a7ade74cf4f0ce83c5e6
Chaeri-Yoon <cryoon0103@gmail.com>
2021-01-24 01:56:31 +0000

    change the way to get a background image
    local-> url

--- src/background.js
+++ src/background.js
@@ -1,9 +1,15 @@
 const body = document.querySelector('body');
 const numOfBackground = 5;
+const imageURLS = ["https://images.pexels.com/photos/1103970/pexels-photo-1103970.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+                    "https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+                "https://images.pexels.com/photos/2002719/pexels-photo-2002719.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+            "https://images.pexels.com/photos/2341830/pexels-photo-2341830.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "https://images.pexels.com/photos/4100130/pexels-photo-4100130.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"];
 
 function setBackground(){
     const image = new Image();
-    image.src = `/background_images/${getRandomNumber()}.jpg`;    
+    //image.src = `/background_images/${getRandomNumber()}.jpg`;    
+    image.src= imageURLS[getRandomNumber()];
     image.classList.add('background__image');
     body.appendChild(image);
 }
